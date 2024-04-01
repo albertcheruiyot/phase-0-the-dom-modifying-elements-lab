7fff3e2aa70b960e971d41ead6daff1e8ef75d2b
Albert Cheruiyot <bettalbert99@gmail.com>
2024-04-01 16:17:19 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id='victory';
+
+const myText = document.createTextNode('Albert is the champion');
+newHeader.appendChild(myText);
